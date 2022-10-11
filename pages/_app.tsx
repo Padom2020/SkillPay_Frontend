5ab3ede4b338579baa0f768e54ea73c4a75dab19
codetective <iaminiodu@gmail.com>
2022-10-11 03:20:17 +0100

    installed typescript, chakra ui setup

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+// pages/_app.js
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+         <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
