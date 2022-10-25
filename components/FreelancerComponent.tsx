f52f5c680b832ad115628e02c4503a2213d87549
milesrykerodazie <milesodazie@gamil.com>
2022-10-25 19:53:57 +0100

    up date on homepage

--- components/FreelancerComponent.tsx
+++ components/FreelancerComponent.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image';
 import React from 'react';
 import { freelancerTRype } from '../interfaces';
+import { BsCheckCircle } from 'react-icons/bs';
+import { BiMessageAltDetail } from 'react-icons/bi';
+import { MdOutlinePersonOutline } from 'react-icons/md';
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 type Props = {};
 
 const FreelancerComponent = ({ freelancer }) => {
    return (
-      <div>
-         <div>
+      <div className='space-y-3 bg-gray-50 p-5 rounded-md'>
+         <div className='flex space-x-2 items-center'>
             <div className='relative w-8 h-8 '>
                <Image
                   src={freelancer?.picture}
@@ -16,29 +20,37 @@ const FreelancerComponent = ({ freelancer }) => {
                />
             </div>
             <div>
-               <p>name</p>
-               <p>job title</p>
+               <p className='font-semibold tracking-wider capitalize text-lg'>
+                  {freelancer?.name}
+               </p>
+               <p className='text-gray-500 capitalize'>{freelancer?.title}</p>
             </div>
          </div>
 
-         <p>description</p>
-         <div>
-            <div>
-               <p>Completed icon</p>
-               <p>Job count and job completed</p>
+         <p className='text-gray-500 text-sm truncate-line-clamp'>
+            {freelancer?.Bio}
+         </p>
+         <div className='flex justify-between'>
+            <div className='flex items-center space-x-1'>
+               <BsCheckCircle className='text-green-600' />
+               <p>1 job completed</p>
             </div>
-            <div>
-               <p>Rating</p>
+            <div className='flex space-x-1'>
+               {[1, 2, 3, 4, 5].map((rate, index) => (
+                  <p key={index}>
+                     <AiOutlineStar className='text-[#FFC303] w-5 h-5' />
+                  </p>
+               ))}
             </div>
          </div>
-         <div>
-            <button>
-               <p>button icon</p>
-               <span>Message</span>
+         <div className='flex space-x-2'>
+            <button className='bg-white ring-1 ring-orange flex items-center space-x-2 justify-center py-1 rounded-sm w-full'>
+               <BiMessageAltDetail className='text-orange' />
+               <span className='text-orange'>Message</span>
             </button>
-            <button>
-               <p>button icon</p>
-               <span>View Profile</span>
+            <button className=' bg-orange ring-1 ring-orange flex items-center justify-center space-x-2 px-2 py-1 rounded-sm w-full'>
+               <MdOutlinePersonOutline className='text-white' />
+               <span className='text-white'>View Profile</span>
             </button>
          </div>
       </div>
--- components/Freelancers.tsx
+++ components/Freelancers.tsx
@@ -7,14 +7,14 @@ type Props = {};
 
 const Freelancers = () => {
    return (
-      <div className='max-w-[70%] mx-auto mt-4'>
+      <div className='w-full px-2 lg:px-0 md:max-w-[70%] mx-auto mt-4'>
          <div className='flex items-center justify-between py-3'>
-            <p className='capitalize font-semibold'>
+            <p className='capitalize font-bold'>
                Check out some expert freelancers
             </p>
             <p className='text-sm text-gray-500'>View all</p>
          </div>
-         <div>
+         <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3'>
             {freelancers?.map((freelancer) => (
                <div key={freelancer?.id}>
                   <FreelancerComponent freelancer={freelancer} />
