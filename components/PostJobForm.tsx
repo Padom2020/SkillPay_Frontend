ccc3553c88837e0d6d804af5fe9a9c4602ea053d
Techydom <drmusic2021@gmail.com>
2022-10-28 18:32:16 +0100

    Post Job Form Completed

--- components/PostJobForm.tsx
+++ components/PostJobForm.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, FormControl, Input, Text, Textarea } from "@chakra-ui/react"
+import { Box, Button, Checkbox, CheckboxGroup, Flex, FormControl, HStack, Input, Stack, Text, Textarea } from "@chakra-ui/react"
 import JobDescEditor from "./JobDescEditor"
 import WrapContent from "./Layout/WrapContent"
 
@@ -15,6 +15,37 @@ const PostJobForm = () => {
         <FormFieldLayout title={"Job description"} desc={"provide a hort decription about the job, keep it hort and to he point"}>
         <JobDescEditor/>
         </FormFieldLayout>
+        <FormFieldLayout title={"Employment type"} desc={"provide a hort decription about the job, keep it hort and to he point"}>
+        <CheckboxGroup>
+        <Stack spacing={"3"}>
+          <Box border={"1px solid"} p="4" borderColor={"gray.200"}>
+          <Checkbox value='Full-time'>Full-time</Checkbox>
+          </Box>
+          <Box border={"1px solid"} p="4" borderColor={"gray.200"}>
+          <Checkbox value='Part-time'>Part-time</Checkbox>
+          </Box>
+          <Box border={"1px solid"} p="4" borderColor={"gray.200"}>
+          <Checkbox value='on-demand'>on demand</Checkbox>
+          </Box>
+        </Stack>
+        </CheckboxGroup>
+        </FormFieldLayout>
+        <FormFieldLayout title={"Working schedule"} desc={"provide a hort decription about the job, keep it hort and to he point"}>
+          <FormControl>
+          <Input type={"number"} placeholder="Estimated Number of days" w={"full"} size="lg"/>
+          </FormControl>
+        </FormFieldLayout>
+        <FormFieldLayout title={"Salary"} desc={"provide a hort decription about the job, keep it hort and to he point"}>
+          <FormControl>
+          <Input type={"number"} placeholder="$1000" w={"full"} size="lg"/>
+          <Checkbox value='Negotiable'>Is it Negotiable?</Checkbox>
+          </FormControl>
+        </FormFieldLayout>
+        <HStack justifyContent={"flex-end"} py="20">
+        <Button  type="submit" bg="#F24726" alignSelf={"center"} w="200px" colorScheme={"orange"}>
+         Login
+        </Button>
+        </HStack>
       </WrapContent>
     </Box>
   )
