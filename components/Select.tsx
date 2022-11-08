b7035dc0ac8d3e2ab6602228fc68923289292325
Emmanuel <isiwuemma@gmail.com>
2022-11-08 06:59:24 +0100

    update

--- components/Select.tsx
+++ components/Select.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { MdPlayArrow } from "react-icons/md";
 
 type AppProps = {
@@ -7,7 +7,7 @@ type AppProps = {
   value?: string,
   onChange?: (evt: React.MouseEvent) => void,
   placeHolder?: string,
-  icon?: React.ReactNode
+  icon?: React.ReactNode,
 }
 const Select = ({children, value, placeHolder, icon, onChange}: AppProps) => {
   const [showOpt, setOpt] = useState(false);
@@ -22,16 +22,20 @@ const Select = ({children, value, placeHolder, icon, onChange}: AppProps) => {
   };
  
   return (
-    <div className="h-8">
+    <div className="md:h-8">
       <div className={classNames("w-full relative border border-[rgba(2, 6, 72, 0.1)] rounded-[8px]", {"border-orange rounded-t-[8px]": showOpt})}>
         <p className={classNames("hover:cursor-pointer h-full pl-12 pt-1 pb-[6px] font-[400] text-[rgba(8, 8, 18, 0.7)] bg-[#F8F9FB] rounded-[8px]", {
           "rounded-t-[8px]": showOpt
         })} onClick={handleShowOpt}>{value?value:placeHolder}</p>
-        <span className={classNames("absolute right-1 rotate-90 top-1 hover:cursor-pointer text-xl", {
-          "text-orange": showOpt
-        })} onClick={handleShowOpt}><MdPlayArrow /></span>
-        <span className="absolute left-3  top-[7px] hover:cursor-pointer text-xl">{icon}</span>
-        {showOpt && <div className="flex flex-col pb-3 mt-1 z-50 bg-[#F8F9FB] overflow-auto rounded-b-[8px]" onClick={handleClick}>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-2">
+            <span className="absolute left-3  top-[7px] hover:cursor-pointer text-xl">{icon}</span>
+            <span className={classNames("absolute right-1 rotate-90 top-1 hover:cursor-pointer text-xl", {
+              "text-orange": showOpt
+            })} onClick={handleShowOpt}><MdPlayArrow /></span>
+          </div>
+        </div>
+        {showOpt && <div className="flex flex-col pb-3 mt-1 z-100 bg-[#F8F9FB] overflow-auto rounded-b-[8px]" onClick={handleClick}>
             {children}
           </div>
         }
--- pages/freelancer/earnings.tsx
+++ pages/freelancer/earnings.tsx
@@ -1,14 +1,45 @@
-import React from "react";
-import { MdPlayArrow } from "react-icons/md";
+import React, {useState} from "react";
+import { MdAccessTime, MdPlayArrow } from "react-icons/md";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { TiArrowRight } from "react-icons/ti";
 import Layout from "../../components/freelancer/Layout";
+import Search from "../../components/Search";
+import { BsBriefcase } from "react-icons/bs";
+import Select from "../../components/Select";
 
 const Earnings = () => {
+  const [searchFilters, setSearchFilters] = useState({
+    textSearch: "",
+    filter1: "",
+    filter2: ""
+  })
+  const [chart, setChart] = useState({
+    options: {
+      chart: {
+        id: 'apexchart-example'
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    },
+    series: [{
+      name: 'Earnings',
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+    }]
+  });
+  const onChange = (filter: string) => (evt: React.MouseEvent) => {
+    const target = evt.target as Element;
+    const targetValue = target.getAttribute("value");
+    setSearchFilters({
+      ...searchFilters,
+      [filter]: targetValue,
+    })
+  }
+
   return (
     <Layout title="SkillPay | Freelancer Earnings">
-      <div className="mt-6 mr-14">
-        <div className="flex flex-col md:flex-row justify-between">
+      <div className="mt-6 px-2 md:px-0 md:mr-14">
+        <div className="flex flex-col md:flex-row items-center md:items-left justify-center space-y-2 md:space-y-0 md:justify-between">
           <h1 className="text-sm md:text-base lg:text-[22px] text-[#080812] font-[500]">Earings <sub className="text-[#948D8D] font-[400] text-[10px] lg:text-[12px]">$8000</sub></h1>
           <div className="flex justify-between items-center h-10 border rounded-[5px] px-2 bg-[#F8F9FB]">
             <div className="flex items-center space-x-2 h-full">
@@ -19,7 +50,7 @@ const Earnings = () => {
           </div>
         </div>
         {/* BADGES */}
-        <div className="grid grid-col-1 md:grid-cols-3 gap-3 w-[90%] my-9">
+        <div className="grid grid-col-1 md:grid-cols-3 gap-3 w-[95%] mx-auto md:mx-0 md:w-[85%] my-9">
           {/* ONE */}
           <div className="text-center border rounded-md pt-4 pb-8">
             <p className="text-gray2 text-xs md:text-sm lg:text-base font-[500]">Earning</p>
@@ -41,6 +72,29 @@ const Earnings = () => {
                 <span>Withdraw</span><span className="text-lg md:text-xl lg:text-2xl mt-1 group-hover:translate-x-2 duration-500"><TiArrowRight /></span>
               </div>
             </div>
+          </div>
+        </div>
+        {/* CHART */}
+        <div className="border rounded-md p-2">
+          <h2 className="flex items-center space-x-3 text-[#170455] font-[700] text-[20px]"><span>Total Earnings</span><span>$8000</span></h2>
+        </div>
+        <div className="w-[100%] md:w-[75%] lg:w-[55%] mt-4 mb-2">
+          <Search placeholder="Search  project" />
+        </div>
+        <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-2">
+          <div className="w-[100%] md:w-[60%] lg:w-[20%]">
+            <Select icon={<BsBriefcase />} value={searchFilters.filter1} onChange={onChange("filter1")} placeHolder="Paid" >
+              <option value="Unpaid" className="cursor-pointer hover:bg-orange pl-3 hover:text-white">Unpaid</option>
+              <option value="Awaiting" className="cursor-pointer hover:bg-orange pl-3 hover:text-white">Awaiting</option>
+              <option value="Pending" className="cursor-pointer hover:bg-orange pl-3 hover:text-white">Paending</option>
+            </Select>
+          </div>
+          <div className="w-[100%] md:w-[60%] lg:w-[20%]">
+            <Select icon={<MdAccessTime />} value={searchFilters.filter2} onChange={onChange("filter2")} placeHolder="Time Period">
+              <option value="Unpaid" className="cursor-pointer hover:bg-orange pl-3 hover:text-white z-100">Unpaid</option>
+              <option value="Awaiting" className="cursor-pointer hover:bg-orange pl-3 hover:text-white">Awaiting</option>
+              <option value="Pending" className="cursor-pointer hover:bg-orange pl-3 hover:text-white">Paending</option>
+            </Select>
           </div>
         </div>
       </div>
