b3749d6e9eb3256d09b2b1920f56ba8312aa13d2
Techydom <drmusic2021@gmail.com>
2022-11-11 11:01:06 +0100

    fmfd

--- next.config.js
+++ next.config.js
@@ -3,11 +3,14 @@ const nextConfig = {
    reactStrictMode: true,
    swcMinify: true,
    images: {
-      domains: [
-         'images.unsplash.com',
-         'plus.unsplash.com',
-         'encrypted-tbn0.gstatic.com',
-      ],
+     domains: [
+       "images.unsplash.com",
+       "plus.unsplash.com",
+       "encrypted-tbn0.gstatic.com",
+     ],
+   },
+   images: {
+     unoptimized: true,
    },
 };
 
